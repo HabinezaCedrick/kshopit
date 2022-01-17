41b2056e3e15e9589b830e4dd65eb743f8479dad
HabinezaCedrick <habinezacedrick8@gmail.com>
2022-01-17 23:10:57 +0200

    make it better

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,220 @@
+import React, { Fragment, useState, useEffect } from 'react'
+import Pagination from 'react-js-pagination'
+import Slider from 'rc-slider'
+import 'rc-slider/assets/index.css';
+
+import MetaData from './layout/MetaData'
+import Product from './product/Product'
+import Loader from './layout/Loader'
+
+import { useDispatch, useSelector } from 'react-redux'
+import { useAlert } from 'react-alert';
+import { getProducts } from '../actions/productActions'
+
+//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+const { createSliderWithTooltip } = Slider;
+const Range = createSliderWithTooltip(Slider.Range)
+
+const Home = ({ match }) => {
+
+    const [currentPage, setCurrentPage] = useState(1)
+    const [price, setPrice] = useState([1, 10000000])
+    const [category, setCategory] = useState('')
+    const [rating, setRating] = useState(0)
+
+    const categories = [
+        'Electronics',
+        'Cameras',
+        'Laptops',
+        'Accessories',
+        'Headphones',
+        'Food',
+        "Books",
+        'Clothes/Shoes',
+        'Beauty/Health',
+        'Sports',
+        'Outdoor',
+        'Home'
+    ]
+
+    const alert = useAlert();
+    const dispatch = useDispatch();
+
+    const { loading, products, error, productsCount, resPerPage, filteredProductsCount } = useSelector(state => state.products)
+
+    const keyword = match.params.keyword
+
+    useEffect(() => {
+        if (error) {
+            return alert.error(error)
+        }
+
+        dispatch(getProducts(keyword, currentPage, price, category, rating));
+
+
+    }, [dispatch, alert, error, keyword, currentPage, price, category, rating])
+
+    function setCurrentPageNo(pageNumber) {
+        setCurrentPage(pageNumber)
+    }
+
+    let count = productsCount;
+    if (keyword) {
+        count = filteredProductsCount
+    }
+
+    return (
+        <Fragment>
+            {loading ? <Loader /> : (
+                <Fragment>
+                    <MetaData title={'Buy Best Products Online'} />
+                    <nav className="navbar row">
+                    <div className="dropdown">
+                   <span className='text-white'><h5 className='fa fa-bars'>   <b>Categories</b></h5></span> 
+                   <div className="dropdown-content">
+                   {categories.map(category => (
+                                                        <li
+                                                            style={{
+                                                                cursor: 'pointer',
+                                                                listStyleType: 'none',
+                                                                hover: 'yellow'
+                                                            }}
+                                                            key={category}
+                                                            onClick={() => setCategory(category)}
+                                                        >
+                                                            {category}
+                                                        </li>
+                                                    ))}
+                   </div>
+                    </div> 
+                   <span className='text-white'><h5 className='fa fa-phone'>   <b>0788997616</b></h5></span> 
+                   <span className='text-white'><h5 className='fa fa-envelope-open'>   <b>shopit@gmail.com</b></h5></span> 
+
+                    </nav>
+
+                
+
+                    <h1 id="products_heading">Latest Products</h1>
+                    <section id="products" className="container mt-5">
+                        <div className="row">
+
+                            {keyword ? (
+                                <Fragment>
+                                    <div className="col-6 col-md-3 mt-5 mb-5">
+                                        <div className="px-3">
+                                            <Range
+                                                marks={{
+                                                    1: `RWF1`,
+                                                    10000000: `RWF10000000`
+                                                }}
+                                                min={1}
+                                                max={10000000}
+                                                defaultValue={[1, 10000000]}
+                                                tipFormatter={value => `RWF${value}`}
+                                                tipProps={{
+                                                    placement: "top",
+                                                    visible: true
+                                                }}
+                                                value={price}
+                                                onChange={price => setPrice(price)}
+                                            />
+
+                                            <hr className="my-5" />
+
+                                            <div className="mt-5">
+                                                <h4 className="mb-3">
+                                                    Categories
+                                                </h4>
+
+                                                <ul className="pl-0">
+                                                    {categories.map(category => (
+                                                        <li
+                                                            style={{
+                                                                cursor: 'pointer',
+                                                                listStyleType: 'none'
+                                                            }}
+                                                            key={category}
+                                                            onClick={() => setCategory(category)}
+                                                        >
+                                                            {category}
+                                                        </li>
+                                                    ))}
+                                                </ul>
+                                            </div>
+
+                                            <hr className="my-3" />
+
+                                            <div className="mt-5">
+                                                <h4 className="mb-3">
+                                                    Ratings
+                                                </h4>
+
+                                                <ul className="pl-0">
+                                                    {[5, 4, 3, 2, 1].map(star => (
+                                                        <li
+                                                            style={{
+                                                                cursor: 'pointer',
+                                                                listStyleType: 'none'
+                                                            }}
+                                                            key={star}
+                                                            onClick={() => setRating(star)}
+                                                        >
+                                                            <div className="rating-outer">
+                                                                <div className="rating-inner"
+                                                                    style={{
+                                                                        width: `${star * 20}%`
+                                                                    }}
+                                                                >
+                                                                </div>
+                                                            </div>
+                                                        </li>
+                                                    ))}
+                                                </ul>
+                                            </div>
+
+                                        </div>
+                                    </div>
+
+                                    <div className="col-7 col-md-8">
+                                        <div className="row">
+                                            {products.map(product => (
+                                                <Product key={product._id} product={product} col={4} />
+                                            ))}
+                                        </div>
+                                    </div>
+                                </Fragment>
+                            ) : (
+                                    products.map(product => (
+                                        <Product key={product._id} product={product} col={3} />
+                                    ))
+                                )}
+
+                        </div>
+                    </section>
+
+                    {resPerPage <= count && (
+                        <div className="d-flex justify-content-center mt-5">
+                            <Pagination
+                                activePage={currentPage}
+                                itemsCountPerPage={resPerPage}
+                                totalItemsCount={productsCount}
+                                onChange={setCurrentPageNo}
+                                nextPageText={'Next'}
+                                prevPageText={'Prev'}
+                                firstPageText={'First'}
+                                lastPageText={'Last'}
+                                itemClass="page-item"
+                                linkClass="page-link"
+                            />
+                        </div>
+                    )}
+
+                </Fragment>
+            )}
+
+        </Fragment>
+    )
+}
+
+export default Home
---
+++ readme.md
@@ -0,0 +1,3 @@
+# ShopIT E-commerce
+
+> Complete E-commerce site built with Node.js, React, Redux, Express, MongoDB.
